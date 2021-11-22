9f2220e7ca8e0e266489541b0b834057139b1bc0
Juan Camilo Sepúlveda Fajardo <juan.kmilo11@hotmail.com>
2021-11-22 17:36:48 -0500

    Changed security groups values

--- security_group.tf
+++ security_group.tf
@@ -2,10 +2,15 @@
 -- Security Groups
 */
 resource "aws_security_group" "sec_group_ecs" {
-  name        = "allow_ecs_inbound_${local.service_name}"
-  description = "Allow inbound"
-  vpc_id      = aws_vpc.vpc_metabase_1.id
+  name   = "sec_gruoup_ecs_${local.service_name}"
+  vpc_id = aws_vpc.vpc_metabase_1.id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     description = "allow connections"
     from_port   = var.port
@@ -14,14 +19,40 @@ resource "aws_security_group" "sec_group_ecs" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "security_group_${local.service_name}"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.vpc_metabase_1.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.sec_group_ecs.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
