fb7e12865756e4fae3a0c9fdee267ea35f05f7ca
Juan Camilo Sepúlveda Fajardo <juan.kmilo11@hotmail.com>
2021-11-24 16:08:45 -0500

    Changed some rules

--- security_group.tf
+++ security_group.tf
@@ -19,20 +19,6 @@ resource "aws_security_group" "sec_group_ecs" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "security_group_${local.service_name}"
   }
@@ -47,12 +33,5 @@ resource "aws_security_group" "rds_sg" {
     to_port         = 3306
     cidr_blocks     = ["0.0.0.0/0"]
     security_groups = [aws_security_group.sec_group_ecs.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
