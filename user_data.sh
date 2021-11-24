46ce0e9ca5d51c5c824ed7d8dd7789c2ef73ebde
Juan Camilo Sepúlveda Fajardo <juan.kmilo11@hotmail.com>
2021-11-24 16:08:29 -0500

    Added tags user_data

--- user_data.sh
+++ user_data.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 sudo apt-get update
-echo "ECS_CLUSTER=${ecs_cluster}" >> /etc/ecs/ecs.config
+echo ECS_CLUSTER=${ecs_cluster} >> /etc/ecs/ecs.config
+echo ECS_CONTAINER_INSTANCE_TAGS={"Name": "ECS-Instance"} >> /etc/ecs/ecs.config
